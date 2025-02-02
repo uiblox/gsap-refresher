70c323ac33b757f75c090767bcb40c7be6cf6cf1
Christian <bournecreative@gmail.com>
2025-02-02 01:14:25 -0600

    import scroll trigger and set up first tween on intro section using scroll position

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; 
+
+gsap.registerPlugin(ScrollTrigger)
+
 function init(){
-    
+  gsap.to("#intro img", {opacity: 0, scrollTrigger: {
+    trigger: '#intro',
+    start: 'top top',
+    end: "bottom center",
+    scrub: true,
+    markers: true
+  }})   
 }
 
 window.addEventListener('load', function(){
