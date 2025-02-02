4425422aecd9cf568168a89f17b026bbc0d29735
Christian <bournecreative@gmail.com>
2025-02-02 02:07:06 -0600

    how to use toggleActions

--- src/main.js
+++ src/main.js
@@ -3,20 +3,46 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger)
 
-function init(){
-  
-  const parallax = gsap.timeline({
-    ease: 'none',
-    scrollTrigger: {
-      trigger: ".bcg-parallax",
-      start: 'top bottom',
-      scrub: true
-    }
+function init() {
+ 
+  const targets = document.querySelectorAll(".project")
+  targets.forEach((target) => {
+     gsap.from(target, {
+      autoAlpha: 0,
+      yPercent: 5,
+      scrollTrigger: {
+        trigger: target.querySelector('img'),
+        start: 'top bottom-=300',
+        end: 'top center',
+        toggleActions: 'play none none reverse'
+      }
+    })
   })
+  // gsap.to(['#intro h1, #intro p'], {
+  //   autoAlpha: 0,
+  //   ease: 'none',
+  //   scrollTrigger: {
+  //     trigger: "#intro .content",
+  //     start: "top top+=5%",
+  //     pin: true,
+  //     scrub: true,
+  //     markers: true
+  //   }
+  // })
 
-  parallax
-    .from('.content-wrapper', { duration: 0.4, autoAlpha: 0}, 0.4)
-    .from('.bcg', { duration: 2, y: '-30%'}, 0)
+  // const parallax = gsap.timeline({
+  //   ease: 'none',
+  //   scrollTrigger: {
+  //     trigger: ".bcg-parallax",
+  //     start: 'top bottom',
+  //     scrub: true
+  //   }
+  // })
+
+
+  // parallax
+  //   .from('.content-wrapper', { duration: 0.4, autoAlpha: 0}, 0.4)
+  //   .from('.bcg', { duration: 2, y: '-30%'}, 0)
   
   // gsap.set('#project02', {scrollTrigger: {
   //   trigger: "#project02",
