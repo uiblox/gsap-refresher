f919c9c12ec6e291d5c2dcd11a9472abb810b1a6
Christian <bournecreative@gmail.com>
2025-02-02 21:24:57 -0600

    toggle class on entry of target section to fade in text

--- src/main.js
+++ src/main.js
@@ -13,13 +13,14 @@ function init() {
       start: "top bottom-=270px",
       end: `+=${document.querySelector(".pen-top").clientHeight + 22}`,
       pin: true,
-      markers: true,
+    //   markers: true,
       pinSpacing: false,
     },
   });
 
   const offsets = [547, 722, 842];
   const targets = [".part4", ".part5", ".part6"];
+  const allParts = gsap.utils.toArray('.part')
 
   offsets.forEach((offset, i) => {
     gsap.set(`${targets[i]}`, { y: `-${offset}` });
@@ -32,7 +33,20 @@ function init() {
         start: "top bottom-=640px",
         end: `+=${offset}`,
         scrub: true,
-        markers: true,
+        // markers: true,
+      },
+    });
+  });
+
+  allParts.forEach((part, i) => {
+    gsap.set(part, {
+      scrollTrigger: {
+        trigger: part,
+        start: i === 3 ? `top+=${document.querySelector(".pen-top").clientHeight + 22}` : "top center",
+        end: `bottom+=${200}`,
+        toggleClass: "fade-in",
+        scrub: true,
+        // markers: true
       },
     });
   });
